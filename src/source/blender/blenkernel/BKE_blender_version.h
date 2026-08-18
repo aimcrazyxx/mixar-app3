53c736231af7c6b341517c0529edcb7ba1c21eb4
aimcrazyxx <jokejoking11@gmail.com>
2026-08-18 14:40:01 -0300

    chore: preserve version header comments

--- src/source/blender/blenkernel/BKE_blender_version.h
+++ src/source/blender/blenkernel/BKE_blender_version.h
@@ -65,7 +65,7 @@ const char *BKE_blender_version_string_compact(void);
 /** Returns true when version cycle is alpha, otherwise (beta, rc) returns false. */
 bool BKE_blender_version_is_alpha(void);
 
-/** As above but does not show patch version. */
+/** Returns true when version suffix is LTS, otherwise returns false. */
 bool BKE_blender_version_is_lts(void);
 
 /**
